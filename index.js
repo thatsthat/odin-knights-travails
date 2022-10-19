b37f36c5a12eb666b387d6674b059a3bfef35d0d
Aram Anto <aram.anto@gmail.com>
2022-10-19 17:37:28 +0200

    pfff :-/

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const Position = (
 
 const Path = (startPos, endPos) => {
   const rootNode = Position(startPos);
-  const finalRoot = nextMoves(rootNode, endPos);
+  const finalRoot = nextMoves(rootNode, 0, endPos);
 
   return { finalRoot };
 
-  function nextMoves(inpNode, endPos) {
-    console.log(inpNode.pos);
+  function nextMoves(inpNode, depth, endPos) {
+    console.log(inpNode.pos, depth);
     const outNode = inpNode;
     const LU = [inpNode.pos[0] - 2, inpNode.pos[1] + 1];
     const LD = [inpNode.pos[0] - 2, inpNode.pos[1] - 1];
@@ -36,34 +36,43 @@ const Path = (startPos, endPos) => {
     if (LU.every(insideBoard)) {
       if (LU == endPos) {
         outNode.LU = Position(LU);
-        console.log("1");
+        //console.log("1");
       } else {
         // console.log("11");
-        outNode.LU = Position(LU, nextMoves(Position(LU), endPos));
+        outNode.LU = Position(LU, nextMoves(Position(LU), depth + 1, endPos));
       }
     }
     if (LD.every(insideBoard)) {
       if (LD == endPos) {
         outNode.LD = Position(LD);
-        console.log("2");
+        //console.log("2");
       } else {
         // console.log("22");
-        outNode.LD = Position(LD, null, nextMoves(Position(LD), endPos));
+        outNode.LD = Position(
+          LD,
+          null,
+          nextMoves(Position(LD), depth + 1, endPos)
+        );
       }
     }
     if (UL.every(insideBoard)) {
       if (UL == endPos) {
         outNode.UL = Position(UL);
-        console.log("3");
+        //console.log("3");
       } else {
         // console.log("33");
-        outNode.UL = Position(UL, null, null, nextMoves(Position(UL), endPos));
+        outNode.UL = Position(
+          UL,
+          null,
+          null,
+          nextMoves(Position(UL), depth + 1, endPos)
+        );
       }
     }
     if (UR.every(insideBoard)) {
       if (UR == endPos) {
         outNode.UR = Position(UR);
-        console.log("4");
+        //console.log("4");
       } else {
         // console.log("44");
         outNode.UR = Position(
@@ -71,14 +80,14 @@ const Path = (startPos, endPos) => {
           null,
           null,
           null,
-          nextMoves(Position(UR), endPos)
+          nextMoves(Position(UR), depth + 1, endPos)
         );
       }
     }
     if (RU.every(insideBoard)) {
       if (RU == endPos) {
         outNode.RU = Position(RU);
-        console.log("5");
+        //console.log("5");
       } else {
         // console.log("55");
         outNode.RU = Position(
@@ -87,7 +96,7 @@ const Path = (startPos, endPos) => {
           null,
           null,
           null,
-          nextMoves(Position(RU), endPos)
+          nextMoves(Position(RU), depth + 1, endPos)
         );
       }
     }
@@ -95,17 +104,17 @@ const Path = (startPos, endPos) => {
       inpNode.RD = Position(RD);
       if (RD == endPos) {
         outNode.RD = Position(RD);
-        console.log("6");
+        //console.log("6");
       } else {
         // console.log("66");
         outNode.RD = Position(RD, null, null);
-        null, null, null, nextMoves(Position(RD), endPos);
+        null, null, null, nextMoves(Position(RD), depth + 1, endPos);
       }
     }
     if (DL.every(insideBoard)) {
       if (DL == endPos) {
         outNode.DL = Position(DL);
-        console.log("7");
+        //console.log("7");
       } else {
         // console.log("77");
         outNode.DL = Position(
@@ -116,14 +125,14 @@ const Path = (startPos, endPos) => {
           null,
           null,
           null,
-          nextMoves(Position(DL), endPos)
+          nextMoves(Position(DL), depth + 1, endPos)
         );
       }
     }
     if (DR.every(insideBoard)) {
       if (DR == endPos) {
         outNode.DR = Position(DR);
-        console.log("8");
+        //console.log("8");
       } else {
         // console.log("88");
         outNode.DR = Position(
@@ -134,7 +143,7 @@ const Path = (startPos, endPos) => {
           null,
           null,
           null,
-          nextMoves(Position(DR), endPos)
+          nextMoves(Position(DR), depth + 1, endPos)
         );
       }
     }
